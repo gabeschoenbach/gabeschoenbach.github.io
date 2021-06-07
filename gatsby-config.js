c75b0cfaf20908d7e7f32d770ee3e692dcdc9ab6
Gabe Schoenbach <gabe.schoenbach@gmail.com>
2021-06-07 16:05:17 -0500

    add image to homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Gabe Schoenbach",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is my only paragraph.</p>
+      <StaticImage
+        alt="This is a 7x7 grid partitioned into seven equal-sized pieces."
+        src="../images/7x7.png"
+      />
     </Layout>
   )
 }
